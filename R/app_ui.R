926f561fbb9b849dff6f3c0183685acdc43205c5
EAMI91 <emiliomorones@gmail.com>
2020-09-15 14:15:37 -0500

    migración a shinydashboard

--- R/app_ui.R
+++ R/app_ui.R
@@ -10,53 +10,56 @@ app_ui <- function(request) {
     # Leave this function for adding external resources
     golem_add_external_resources(),
     # Dashboard
-    dashboardPage(title = h1("Tablero electoral"),
-                  tags$head(
-                    includeCSS(app_sys("app/www/tElectoral.css")),
-                    tags$link(href="https://fonts.googleapis.com/css?family=Muli:400,600,700&display=swap", rel = "stylesheet"),
-                    tags$script(async = TRUE, src = "https://platform.twitter.com/widgets.js")
-                  ),
-                  navbar = dashboardHeader(),
-                  sidebar = dashboardSidebar(expand_on_hover = F,
-                    # Sidebar #####
-                    sidebarMenu(
-                      menuItem("Inicio",
-                               tabName = "inicio", 
-                               icon = icon("dashboard")),
-                      menuItem("Investigación",
-                               tabName = "investigacion", 
-                               icon = icon("dashboard")),
-                      menuItem("Protocolo de cuestionarios",
-                               tabName = "comunicacion", 
-                               icon = icon("dashboard")),
-                      menuItem("Gira", 
-                               tabName = "gira",
-                               icon = icon("th")),
-                      menuItem("Finanzas",
-                               tabName = "finanzas", 
-                               icon = icon("dashboard")),
-                      menuItem("Legal", 
-                               tabName = "legal",
-                               icon = icon("th")),
-                      menuItem("Organización",
-                               tabName = "organizacion", 
-                               icon = icon("dashboard")),
-                      menuItem("Estructura", 
-                               tabName = "estructura",
-                               icon = icon("th"))),
-                    # Cuerpo #######
-                  ),
-                  body =dashboardBody(
-                    tabItems(
-                      tabItem(tabName = "inicio", 
-                              mod_portada_ui("portada_ui_1")),
-                      tabItem(tabName = "investigacion",
-                              mod_investigacionAnalisis_ui("investigacionAnalisis_ui_1")),
-                      tabItem(tabName = "comunicacion",
-                              mod_comunicacion_ui("comunicacion_ui_1"))
-                      
-                    )
-                  )
+    dashboardPage(
+      # tags$head(
+      #   includeCSS(app_sys("app/www/tElectoral.css")),
+      #   tags$link(href="https://fonts.googleapis.com/css?family=Muli:400,600,700&display=swap", rel = "stylesheet"),
+      #   tags$script(async = TRUE, src = "https://platform.twitter.com/widgets.js")
+      # ),
+      header = dashboardHeader(title = "Tablero electoral"),
+      sidebar = dashboardSidebar(#expand_on_hover = F,
+        # Sidebar #####
+        sidebarMenu(
+          menuItem("Inicio",
+                   tabName = "inicio", 
+                   icon = icon("dashboard")
+          ),
+          menuItem("Investigación",
+                   tabName = "investigacion",
+                   icon = icon("dashboard")),
+          menuItem("Protocolo de cuestionarios",
+                   tabName = "comunicacion",
+                   icon = icon("dashboard")),
+          menuItem("Gira",
+                   tabName = "gira",
+                   icon = icon("th")),
+          menuItem("Finanzas",
+                   tabName = "finanzas",
+                   icon = icon("dashboard")),
+          menuItem("Legal",
+                   tabName = "legal",
+                   icon = icon("th")),
+          menuItem("Organización",
+                   tabName = "organizacion",
+                   icon = icon("dashboard")),
+          menuItem("Estructura",
+                   tabName = "estructura",
+                   icon = icon("th")
+          )
+        )
+        # Cuerpo #######
+      ),
+      body =dashboardBody(
+        tabItems(
+          tabItem(tabName = "inicio", 
+                  mod_portada_ui("portada_ui_1")),
+          tabItem(tabName = "investigacion",
+                  mod_investigacionAnalisis_ui("investigacionAnalisis_ui_1")),
+          tabItem(tabName = "comunicacion",
+                  mod_comunicacion_ui("comunicacion_ui_1"))
+          
+        )
+      )
     )
   )
 }
@@ -74,7 +77,7 @@ golem_add_external_resources <- function(){
   add_resource_path(
     'www', app_sys('app/www')
   )
- 
+  
   tags$head(
     favicon(),
     bundle_resources(
