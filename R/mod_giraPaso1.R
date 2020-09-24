b91b59b85ee908a5b80905224e09d00ba41ca8ec
Jesús Alberto Sánchez Selvas <33760262+JesuSelvaS@users.noreply.github.com>
2020-09-24 10:52:50 -0500

    ui, falta tibble

--- R/mod_giraPaso1.R
+++ R/mod_giraPaso1.R
@@ -10,7 +10,32 @@
 mod_giraPaso1_ui <- function(id){
   ns <- NS(id)
   tagList(
- 
+    h3("Instancia de gira"),
+    p("A continuación, llene los campos para completar el paso 1"),
+    fluidRow(
+      column(width = 12,
+             textInput(inputId = ns("Responsable"), label = "Responsable" , placeholder = "..."))
+    ),
+    fluidRow(
+      column(width = 12,
+             textAreaInput(inputId = ns("Descripcion"), label = "Descripcion" , placeholder = "...", rows = 5))
+    ),
+    fluidRow(
+      column(width = 8,
+             selectizeInput(inputId = ns("LugarInicio"), label = "Lugar de inicio", choices = c("Seleccione un lugar" = "", "Lugar 1", "Lugar 2") )
+             ),
+      column(width = 4,
+             timeInput(inputId = ns("HorarioInicio"), label = "Hora de inicio", value =  Sys.time(), seconds = F)
+      )
+    ),
+    fluidRow(
+      column(width = 8,
+             selectizeInput(inputId = ns("LugarFinal"), label = "Lugar de destino", choices = c("Seleccione un lugar" = "", "Lugar 1", "Lugar 2") )
+      ),
+      column(width = 4,
+             timeInput(inputId = ns("HorarioFinal"), label = "Hora de finalización", value =  Sys.time(), seconds = F)
+      )
+    )
   )
 }
     
