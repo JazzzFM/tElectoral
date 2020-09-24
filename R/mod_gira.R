937f7167ac9070b53928d685460844b85a81ba94
EAMI91 <emiliomorones@gmail.com>
2020-09-24 12:57:47 -0500

    guardar paso1

--- R/mod_gira.R
+++ R/mod_gira.R
@@ -12,8 +12,7 @@ mod_gira_ui <- function(id){
   tagList(
     tabsetPanel(
       tabPanel(title = "Paso 1",
-        mod_giraPaso1_ui(ns("giraPaso1_ui_1")),
-        actionButton(inputId = ns("GuardarPaso1"), "Guardar")
+               mod_giraPaso1_ui(ns("giraPaso1_ui_1"))
       ),
       tabPanel(title = "Paso 3",
                mod_giraPaso3_ui(ns("giraPaso3_ui_1"))
@@ -21,24 +20,23 @@ mod_gira_ui <- function(id){
     )
   )
 }
-    
+
 #' gira Server Function
 #'
 #' @noRd 
 mod_gira_server <- function(input, output, session){
   ns <- session$ns
-  paso1 <- callModule(mod_giraPaso1_server, "giraPaso1_ui_1")
+  
   # Paso 1
-  observeEvent(input$GuardarPaso1, {
-    print(paso1())
-  })
+  paso1 <- callModule(mod_giraPaso1_server, "giraPaso1_ui_1")
+  
   # Paso 3
   callModule(mod_giraPaso3_server, "giraPaso3_ui_1")
 }
-    
+
 ## To be copied in the UI
 # 
-    
+
 ## To be copied in the server
 # 
- 
+
--- R/mod_giraPaso1.R
+++ R/mod_giraPaso1.R
@@ -23,7 +23,7 @@ mod_giraPaso1_ui <- function(id){
     fluidRow(
       column(width = 8,
              selectizeInput(inputId = ns("LugarInicio"), label = "Lugar de inicio", choices = c("Seleccione un lugar" = "", "Lugar 1", "Lugar 2") )
-             ),
+      ),
       column(width = 4,
              timeInput(inputId = ns("HorarioInicio"), label = "Hora de inicio", value =  Sys.time(), seconds = F)
       )
@@ -35,21 +35,41 @@ mod_giraPaso1_ui <- function(id){
       column(width = 4,
              timeInput(inputId = ns("HorarioFinal"), label = "Hora de finalización", value =  Sys.time(), seconds = F)
       )
-    )
+    ),
+    actionButton(inputId = ns("guardar"), "Guardar")
   )
 }
-    
+
 #' giraPaso1 Server Function
 #'
 #' @noRd 
 mod_giraPaso1_server <- function(input, output, session){
   ns <- session$ns
-  return (reactive(tibble::tibble(Responsable = input$Responsable, Descripcion = input$Descripcion, LugarInicio = input$LugarInicio, HorarioInicio = input$HorarioInicio, LugarFinal = input$LugarFinal, HorarioFinal = input$HorarioFinal)))
+  
+  gira <- reactiveVal()
+  
+  observeEvent(input$guardar,{
+    browser()
+    check <- c("Responsable","Descripcion","LugarInicio","HorarioInicio","LugarFinal","HorarioFinal") %>%
+      mandatory(input = input, .)
+    if(check){
+      gira <- tibble::tibble(
+        Responsable = input$Responsable, 
+        Descripcion = input$Descripcion, LugarInicio = input$LugarInicio, 
+        HorarioInicio = input$HorarioInicio, LugarFinal = input$LugarFinal, 
+        HorarioFinal = input$HorarioFinal
+      ) 
+    } else{
+      shinyalert::shinyalert(title = "Formato incompleto")
+    }
+  })
+  
+  return(gira)
 }
-    
+
 ## To be copied in the UI
 # 
-    
+
 ## To be copied in the server
 # 
- 
+
