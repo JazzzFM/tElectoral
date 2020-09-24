567a9a8cb81179faff94e12fdd3273962563b323
EAMI91 <emiliomorones@gmail.com>
2020-09-24 13:56:48 -0500

    reactividad del paso2

--- R/mod_lugaresGira.R
+++ R/mod_lugaresGira.R
@@ -22,39 +22,42 @@ mod_lugaresGira_ui <- function(id){
     )
   )
 }
-    
+
 #' lugaresGira Server Function
 #'
 #' @noRd 
 mod_lugaresGira_server <- function(input, output, session){
   ns <- session$ns
-  # Temporal seleccion de municipios al azar
-  mun_sel <- DB_Mich2$CABECERA_MUNICIPAL %>% sample(size = 3)
-  a <- camino_mas_corto(municipios_seleccionados = mun_sel,
-                        info=munRPAP,
-                        municipios = DB_Mich2)
+  muns <- reactive({
+    DB_Mich2 %>% select(CABECERA_MUNICIPAL, TOTAL_VOTOS)
+  })
+  a <- reactive({
+    camino_mas_corto(municipios_seleccionados = muns()%>% slice(input$recomendaciones_rows_selected) %>% 
+                       pull(CABECERA_MUNICIPAL),
+                     info=munRPAP,
+                     municipios = DB_Mich2)
+  })
   # Mapa de gira
   output$mapa <- renderLeaflet({
-    a[[1]]
-  }
-  )
+    req(a()[[1]])
+  })
   
   # Info de la gira
   output$info <- renderUI({
-    a[[2]] %>% paste(collapse = "\n")
+    req(a()[[2]]) %>% paste(collapse = "\n")
   })
   
   # Tabla de recomendaciones
   output$recomendaciones <- DT::renderDT({
-    DB_Mich2 %>% select(CABECERA_MUNICIPAL, TOTAL_VOTOS)
+    muns()
   })
   
-
+  
 }
-    
+
 ## To be copied in the UI
 # mod_lugaresGira_ui("lugaresGira_ui_1") # Listo
-    
+
 ## To be copied in the server
 # callModule(mod_lugaresGira_server, "lugaresGira_ui_1") # Listo
- 
+
