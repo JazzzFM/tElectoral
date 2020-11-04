7034fb60e23f6b9e0488631cb70170f961b8cf0b
Jaziel Flores <jazzzfm@protonmail.com>
2020-11-03 18:22:05 -0600

    Grafica de poll of polls con asignación de colores pendiente

--- R/mod_investigacionAnalisis_fct_graficos.R
+++ R/mod_investigacionAnalisis_fct_graficos.R
@@ -92,8 +92,8 @@ procesamiento_graph <- function(DB){
     full_join(y = Vari, by = "fecha")
   
   BB <- BB %>% mutate(fecha = dmy(fecha),
-                      min = votacion - var,
-                      max = votacion + var)
+                      min = votacion - var/sqrt(30),
+                      max = votacion + var/sqrt(30))
   
   BB <- BB %>% arrange(fecha)
   BB <- BB %>% filter(!candidato %in% c('Aún no sabe',
@@ -104,6 +104,16 @@ procesamiento_graph <- function(DB){
                                         'Indefinidos','Ninguno',
                                         'Anulará su voto',
                                         'No ha tomado una decisión'))
+  
+  BB <- filter(BB, votacion > 0.10)
+
+  BAUX = tibble(candidato = c("PRI", "PAN", "MORENA", "PRD", "PES", "PVME",
+                              "PT", "MC", "INDEPENDIENTE"), 
+                colores = c("#00A453", "#00539B", "#600B10", "#FED90E",
+                           "#7030A0", "#FD2017", "#00B83A", "#F05606",
+                           "#E29578"))
+  
+  BB <- BB %>% full_join(y = BAUX, by = "candidato")
 
   return(BB)
 }
@@ -132,36 +142,38 @@ hPollofPolls <- function(DB){
   DB <-DB %>% mutate(votacion_r = round(votacion*100),
                      votacion_min = round(min*100),
                      votacion_max = round(max*100),
-                     votacion = votacion *100,
+                     votacion = votacion*100,
                      min = min * 100,
                      max = max * 100)
   # Tooltip
   tt <- tooltip_table(c("{point.series.name}"),
-                      c("{point.votacion_r}%"))
+                      c("{point.votacion_r} %"))
   # Gráfica
-  Graph <- DB%>% 
+  Graph <- DB %>% 
     hchart(hcaes(x = fecha,  low = min,
-                              high = max, group = candidato),
-                        type = "arearange", enableMouseTracking= F)%>% 
+                 high = max, group = candidato, fill = candidato, color = colores),
+                 type = "arearange", fillOpacity = 0.15, 
+                 enableMouseTracking = F)%>%
+    hc_colors(colores) %>% 
     hc_title(text = "Poll of Polls") %>%
     hc_subtitle(text = "Data from Different Survey Houses") %>% 
     hc_add_series(data = DB,
                   hcaes(x = fecha, y = votacion,
-                        group = candidato),
-                  type = "line") %>% 
+                        group = candidato, fill = candidato, color = colores),
+                        type = "line") %>% 
+    hc_colors(colores) %>% 
     hc_yAxis(title = list(text = "Porcentaje"), labels = list(format = "{value}%") ) %>%
     hc_xAxis(crosshair = T) %>% 
     hc_plotOptions(line = list(colorByPoint = F, showInLegend = F)) %>% 
-    hc_tooltip(sort = T,
-               share=T,
-               borderWidth= 0,
-               split=T,
+    hc_tooltip(sort = F,
+               share = F,
+               borderWidth = 0,
+               split = T,
                pointFormat = tt, 
                useHTML = TRUE) %>%
     hc_add_theme(hc_theme_hcrt()) %>%
-    hc_legend(enabled = TRUE) %>% 
-    hc_colors(c("#685369","#849324", "#F7ACCF", "#4E8098", "#767347", "BEA07A", "6F6358", "#A396B4", "#798BA6"))
-  
+     hc_legend(enabled = TRUE)
+    
   return(Graph)
 }
 
