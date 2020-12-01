d48735feabc7125a667dbea0d52e7c1273e07067
Jesús Alberto Sánchez Selvas <33760262+JesuSelvaS@users.noreply.github.com>
2020-12-01 10:07:03 -0600

    Diseño implementado en form general, dis muestral e intencion de voto

--- R/mod_investigacionFormularioDisMuestral.R
+++ R/mod_investigacionFormularioDisMuestral.R
@@ -17,84 +17,83 @@ mod_investigacionFormularioDisMuestral_ui <- function(id){
     ),
     h3("Formulario de diseño muestral "),
     tags$hr(),
-    p("Llene los siguientes campos ..."),
-    fluidRow(
-      column(width = 4,
-             dateInput(inputId = ns("fechaRegistro"), label = "Fecha de registo",
-                       format = "dd/mm/yyyy", language = "es", value = Sys.Date(),
-                       min = Sys.Date())
-      ),
-      column(width = 4,
-             pickerInput(label = "Modo de levantamiento",
-                         choices = c("Seleccione" = '',"Vivienda","Telefónica","Internet"),
-                         inputId = ns("modoLevantamiento"), selected = 0)
-             ),
-      column(width = 4,
-              textInput(inputId = ns("marcoMuestral"), label = "Marco Muestral",
-                        placeholder = "Respuesta libre ...")
-            ),
-      column(width = 6, 
-             numericInput(inputId = ns("numeroEntrevistas"),
+    div(class="shadowForm",
+        fluidRow(
+          
+          column(width = 4,
+                 pickerInput(label = "Modo de levantamiento",
+                             choices = c("Seleccione" = '',"Vivienda","Telefónica","Internet"),
+                             inputId = ns("modoLevantamiento"), selected = 0)
+          ),
+          column(width = 4,
+                 numericInput(inputId = ns("numeroEntrevistas"),
                               label = "Número de entrevistas",
                               value = 0,
                               min = 0)
-            ),
-      column(width = 6,
-        prettyRadioButtons(label = "Aleatoria", choices = c("Sí", "No"),
-                           inputId = ns("aleatoria"), selected = 0)
-      ),
-      column(width = 6,
-             prettyRadioButtons(label = "Poloetápica", choices = c("Sí", "No"),
-                                inputId = ns("poliEtapa"), selected = 0)
-      ),
-      column(width = 6,
-      shinyjs::hidden(numericInput(inputId = ns("nivelpoliEtapa"),
-                   label = "¿Cuántos niveles?",
-                   value = 1,
-                   min = 1,
-                   max = 10))
+          ),
+          column(width = 4,
+                 prettyRadioButtons(label = "Aleatoria", choices = c("Sí", "No"),
+                                    inputId = ns("aleatoria"), selected = 0)
+          ),
+          column(width = 12, 
+                 textInput(inputId = ns("marcoMuestral"), label = "Marco Muestral",
+                           placeholder = "Respuesta libre ...")
+          ),
+          
+          column(width = 3,
+                 prettyRadioButtons(label = "Poloetápica", choices = c("Sí", "No"),
+                                    inputId = ns("poliEtapa"), selected = 0)
+          ),
+          column(width = 3,
+                 shinyjs::hidden(numericInput(inputId = ns("nivelpoliEtapa"),
+                                              label = "¿Cuántos niveles?",
+                                              value = 1,
+                                              min = 1,
+                                              max = 10))
+          ),
+          column(width = 3,
+                 prettyRadioButtons(label = "Estratificada", choices = c("Sí", "No"),
+                                    inputId = ns("estratificada"), selected = 0)
+          ),
+          column(width = 3,
+                 shinyjs::hidden(numericInput(inputId = ns("nivielEstratificada"),
+                                              label = "¿Cuántos niveles?",
+                                              value = 1,
+                                              min = 1,
+                                              max = 10))
+          ),
+          column(width = 3,
+                 prettyRadioButtons(label = "Conglomerados", choices = c("Sí", "No"),
+                                    inputId = ns("conglomerados"), selected = 0)
+          ),
+          column(width = 3,
+                 shinyjs::hidden(numericInput(inputId = ns("nivielConglomerados"),
+                                              label = "¿Cuántos niveles?",
+                                              value = 1,
+                                              min = 1,
+                                              max = 10))
+          ),
+          column(width = 12,
+                 textInput(inputId = ns("unidadMuestral"), label = "Unidad muestral",
+                           placeholder = "Respuesta libre ...")
+          ),
+          column(width = 12,
+                 textInput(inputId = ns("nivelConfianza"), label = "Nivel de confianza",
+                           placeholder = "Respuesta libre ...")
+          ),
+          column(width = 12,
+                 textInput(inputId = ns("margenError"), label = "Margen de error",
+                           placeholder = "Respuesta libre ...")
+          ),
+          column(width = 12,
+                 textAreaInput(inputId = ns("observaciones"), label = 'Observaciones', rows = 6,placeholder = "Respuesta libre ..."),
+          ),
         ),
-      column(width = 6,
-             prettyRadioButtons(label = "Estratificada", choices = c("Sí", "No"),
-                                inputId = ns("estratificada"), selected = 0)
-      ),
-      column(width = 6,
-             shinyjs::hidden(numericInput(inputId = ns("nivielEstratificada"),
-                                          label = "¿Cuántos niveles?",
-                                          value = 1,
-                                          min = 1,
-                                          max = 10))
-      ),
-      column(width = 6,
-             prettyRadioButtons(label = "Conglomerados", choices = c("Sí", "No"),
-                                inputId = ns("conglomerados"), selected = 0)
-      ),
-      column(width = 6,
-             shinyjs::hidden(numericInput(inputId = ns("nivielConglomerados"),
-                                          label = "¿Cuántos niveles?",
-                                          value = 1,
-                                          min = 1,
-                                          max = 10))
-      ),
-      column(width = 12,
-             textInput(inputId = ns("unidadMuestral"), label = "Unidad muestral",
-                       placeholder = "Respuesta libre ...")
-      ),
-      column(width = 12,
-             textInput(inputId = ns("nivelConfianza"), label = "Nivel de confianza",
-                       placeholder = "Respuesta libre ...")
-      ),
-      column(width = 12,
-             textInput(inputId = ns("margenError"), label = "Margen de error",
-                       placeholder = "Respuesta libre ...")
-      ),
-      column(width = 12,
-             textAreaInput(inputId = ns("observaciones"), label = 'Observaciones', placeholder = "Respuesta libre ..."),
-      ),
+        hr(),
+        fluidRow(
+          uiOutput(ns("outGuardar"), style = "width: 100%")
+        )
     ),
-    fluidRow(
-      uiOutput(ns("outGuardar"))
-    )
   )
 }
     
@@ -211,15 +210,37 @@ mod_investigacionFormularioDisMuestral_server <- function(input, output, session
   output$outGuardar <- renderUI({
     if(readOnly$val == FALSE){
       tagList(
-        column(width = 6,
-               actionButton(inputId = ns("agregarCareo"), label = "Guardar y agregar otro", class ="btn-default")
-        ),
-        column(width = 6,
-               actionButton(inputId = ns("guardar"), label = "Guardar y terminar", class ="btn-primary pull-right")
+        fluidRow(class="padding15-25",
+          column(width = 6,
+                 actionButton(inputId = ns("agregarCareo"), label = "Guardar y agregar otro", class ="btn-default")
+          ),
+          column(width = 6,
+                 actionButton(inputId = ns("guardar"), label = "Guardar y terminar", class ="btn-primary pull-right")
+          )
         )
       )
     }
-  }) 
+  })
+  infoDisMuestral <- reactiveVal(NULL)
+  observe({
+    if(idDMuestral$val != 0){
+      infoDisMuestral(bd$listadoDisMuestral %>% filter(activo == 1 & idFormGeneral == !! idFormGeneral$val & idDMuestral == !! idDMuestral$val) %>% collect())
+      updatePickerInput(session = parent_session, inputId = ns("modoLevantamiento"), selected = infoDisMuestral()$modoLevantamiento)
+      updateTextInput(session = parent_session, inputId = ns("marcoMuestral"), value = infoDisMuestral()$marcoMuestral)
+      updateNumericInput(session = parent_session, inputId = ns("numeroEntrevistas"), value = infoDisMuestral()$numeroEntrevistas)
+      updatePrettyRadioButtons(session = parent_session, inputId = ns("aleatoria"), selected = infoDisMuestral()$aleatoria)
+      updatePrettyRadioButtons(session = parent_session, inputId = ns("poliEtapa"), selected = infoDisMuestral()$poliEtapa)
+      updateNumericInput(session = parent_session, inputId = ns("nivelpoliEtapa"), value = infoDisMuestral()$nivelpoliEtapa)
+      updatePrettyRadioButtons(session = parent_session, inputId = ns("estratificada"), selected = infoDisMuestral()$estratificada)
+      updateNumericInput(session = parent_session, inputId = ns("nivielEstratificada"), value = infoDisMuestral()$nivielEstratificada)
+      updatePrettyRadioButtons(session = parent_session, inputId = ns("conglomerados"), selected = infoDisMuestral()$conglomerados)
+      updateNumericInput(session = parent_session, inputId = ns("nivielConglomerados"), value = infoDisMuestral()$nivielConglomerados)
+      updateTextInput(session = parent_session, inputId = ns("unidadMuestral"), value = infoDisMuestral()$unidadMuestral)
+      updateTextInput(session = parent_session, inputId = ns("nivelConfianza"), value = infoDisMuestral()$nivelConfianza)
+      updateTextInput(session = parent_session, inputId = ns("margenError"), value = infoDisMuestral()$margenError)
+      updateTextAreaInput(session = parent_session , inputId = ns("observaciones"), value = infoDisMuestral()$observaciones)
+    }
+  })
 }
     
 ## To be copied in the UI
