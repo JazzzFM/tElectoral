7ea65e00620ea649fc6ec0a512b6c06617d7e2bf
Jesús Alberto Sánchez Selvas <33760262+JesuSelvaS@users.noreply.github.com>
2020-12-04 17:55:38 -0600

    Estilo login, avance en cuestionario

--- R/mod_cuestionario_paso_1.R
+++ R/mod_cuestionario_paso_1.R
@@ -20,25 +20,25 @@ mod_cuestionario_paso_1_ui <- function(id){
     div(class="shadowForm",
         fluidRow(
           column(width = 12,
-                 fileInput(inputId = ns("adjunto"), label = "Adjunte en PDF el archivo sujeto a análisis", accept = "application/pdf",buttonLabel = icon("search"))
+                 fileInput(inputId = ns("urlArchivo"), label = "Adjunte en PDF el archivo sujeto a análisis", accept = "application/pdf",buttonLabel = icon("search"))
           ),
           column(width = 6,
                  pickerInput(inputId = ns("nivelClaridad"), label = "¿Cuál es el nivel de claridad de los objetivos de investigación?", choices = c("Seleccione" = "" , "Alta", "Intermedio", "Bajo", "No hay"))
           ),
           column(width = 6,
-                 textAreaInput(inputId = ns("observacionClaridad"), label = "Observaciones", value = "", rows = 5, placeholder = "(Opcional)")
+                 textAreaInput(inputId = ns("obsNivelClaridad"), label = "Observaciones", value = "", rows = 5, placeholder = "(Opcional)")
           ),
           column(width = 6,
-                 pickerInput(inputId = ns("opBloquesCuestionario"), label = "¿Se operacionalizan todos los objetivos de investigación en los bloques del cuestionario?", choices = c("Seleccione" = "" , "Sí, completos", "Sí, parcialmente", "No"))
+                 pickerInput(inputId = ns("operacionalizacion"), label = "¿Se operacionalizan todos los objetivos de investigación en los bloques del cuestionario?", choices = c("Seleccione" = "" , "Sí, completos", "Sí, parcialmente", "No"))
           ),
           column(width = 6,
                  textAreaInput(inputId = ns("observacionOpBloquesCuestionario"), label = "Observaciones", value = "", rows = 5, placeholder = "(Opcional)")
           ),
           column(width = 6,
-                 pickerInput(inputId = ns("filtrosPoblacionObjetivo"), label = "¿Hay filtros que permitan identificar correctamente a la población objetivo?", choices = c("Seleccione" = "" , "Sí, completos", "Sí, parcialmente", "No"))
+                 pickerInput(inputId = ns("poblacionObjetivo"), label = "¿Hay filtros que permitan identificar correctamente a la población objetivo?", choices = c("Seleccione" = "" , "Sí, completos", "Sí, parcialmente", "No"))
           ),
           column(width = 6,
-                 textAreaInput(inputId = ns("observacionFiltrosPoblacionObjetivo"), label = "Observaciones", value = "", rows = 5, placeholder = "(Opcional)")
+                 textAreaInput(inputId = ns("obsPoblacionObjetivo"), label = "Observaciones", value = "", rows = 5, placeholder = "(Opcional)")
           )
         ),
         hr(),
@@ -53,80 +53,29 @@ mod_cuestionario_paso_1_ui <- function(id){
 #' @noRd 
 mod_cuestionario_paso_1_server <- function(input, output, session, cuestionario = c()){
   ns <- session$ns
-  # observeEvent(input$ConBasicos,{
-  #   removeUI(
-  #     selector = ".ColumnEspecifique"
-  #   ) 
-  #   for (value in input$ConBasicos) {
-  #     if(value == "Otro"){
-  #       insertUI(
-  #         selector = '.RowInvestigacion', 
-  #         where = "beforeEnd",
-  #         ui = column(width = 6, 
-  #                     class="ColumnEspecifique",
-  #                     textInput(inputId = "Especifique", placeholder = "...", label = "Especifique"),
-  #                     actionButton(inputId = ns("CancelarOtro"), "Cancelar", class = "btn-default"),
-  #                     actionButton(inputId = "Guardar", "Guardar concepto", class = "btn-primary"),
-  #         )
-  #       )
-  #       break;
-  #     }
-  #   }
-  # }, ignoreNULL = F)
-  # observeEvent(input$CancelarOtro, {
-  #   removeUI(
-  #     selector = ".ColumnEspecifique"
-  #   )
-  #   # x <- input$ConBasicos
-  #   # remove <- c("Otro")
-  #   # setdiff(x, remove)
-  #   #updateSelectizeInput(session = session, inputId = ns("ConBasicos"), selected = NULL, server = T)
-  # })
-  # Guardar cuestionario
-  
+
   observeEvent(input$GuardarPaso1, {
-    #shinyjs::disable(input$GuardarPaso1)
-    check <- c("nombreCuestionario", "nivelClaridad",  "opBloquesCuestionario",  "filtrosPoblacionObjetivo") %>% mandatory(input = input)
+    shinyjs::disable(input$GuardarPaso1)
+    check <- c("nivelClaridad",  "operacionalizacion",  "poblacionObjetivo") %>% mandatory(input = input)
     if(!check){
       shinyalert::shinyalert("Incompleto", "Favor de llenar todas las entradas.")
     }else{
       # Tibble
       cuestionario$paso1 <- tibble::tibble(
-        nombreCuestionario = input$nombreCuestionario,
-        adjunto = "adjunto",
+        urlArchivo = "adjunto",
         nivelClaridad = input$nivelClaridad,
-        observacionClaridad = input$observacionClaridad,
-        opBloquesCuestionario = input$opBloquesCuestionario,
-        observacionOpBloquesCuestionario = input$observacionOpBloquesCuestionario,
-        filtrosPoblacionObjetivo = input$filtrosPoblacionObjetivo,
-        observacionFiltrosPoblacionObjetivo = input$observacionFiltrosPoblacionObjetivo,
-        cantidadBloques = 0
+        obsNivelClaridad = input$obsNivelClaridad,
+        operacionalizacion = input$operacionalizacion,
+        obsOperacionalizacion = input$obsOperacionalizacion,
+        poblacionObjetivo = input$poblacionObjetivo,
+        obsPoblacionObjetivo = input$obsPoblacionObjetivo,
+        cantidadBloques = 0,
+        obsGenerales = "",
+        correo = ""
       )
-      print(cuestionario$paso1)
     }
-    #shinyjs::enable(input$GuardarPaso1)
+    shinyjs::enable(input$GuardarPaso1)
   })
-  
-  # Generar bloques
-  # observeEvent(input$genBloques, {
-  #   value <- input$CantidadBloques
-  #   removeUI(
-  #     selector = ".NombreBloque",
-  #     multiple = T
-  #   ) 
-  #   if(value > 0){
-  #     for (item in 1:value) {
-  #       insertUI(
-  #         selector = '.NombreBloquesContainer', 
-  #         where = "beforeEnd",
-  #         ui = column(class="NombreBloque",
-  #                     width = 6, 
-  #                     textInput(inputId = ns(paste0("NombreBloque-", item)), placeholder = "...", label = paste0("Título de bloque no. ", item))
-  #         )
-  #       )
-  #     }
-  #   }
-  # })
 }
     
 ## To be copied in the UI
